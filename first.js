81f77f8df58dd35e0c433d80df96065cbd81dfa9
ankit-yadav90 <ankityadav73699@gmail.com>
2025-03-07 15:16:05 +0530

    else if statement

---
+++ first.js
@@ -0,0 +1,16 @@
+//Conditinal Statement
+
+let mode = "dark";
+let color;
+
+if (mode === "dark") {
+    color = "black";
+} else if (mode === "blue") {
+    color = "red";
+} else if (mode === "pink") {
+    color = "green";
+}else {
+    color = "white";
+}
+
+console.log(color);
